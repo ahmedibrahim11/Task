70e81b455ea8e9bba7c4c0113c72d33d88f1cea8
AbdElKhalekA <AbdElKhalekA@E0410830>
2018-12-16 23:33:43 +0200

    unit-test-for userdetailsComponent

--- src/modules/components/user-details.js
+++ src/modules/components/user-details.js
@@ -8,12 +8,12 @@ class UserDetails extends React.Component {
     };
     render() {
         return (
-            <div class="card" style={{ width: '18rem', marginLeft: 250, marginTop: 55 }}>
-                <img class="card-img-top" src={this.props.currentUser.avatar_url} alt="" />
-                <div class="card-body">
-                    <p class="card-text">Name:{this.props.currentUser.name}</p>
-                    <p class="card-text">Email:{this.props.currentUser.email}</p>
-                    <p class="card-text">Date:{this.props.currentUser.created_at}</p>
+            <div className="card" style={{ width: '18rem', marginLeft: 250, marginTop: 55 }}>
+                <img className="card-img-top" src={this.props.currentUser.avatar_url} alt="" />
+                <div className="card-body" >
+                    <p id="name" className="card-text">Name:{this.props.currentUser.name}</p>
+                    <p className="card-text">Email:{this.props.currentUser.email}</p>
+                    <p className="card-text">Date:{this.props.currentUser.created_at}</p>
                 </div>
             </div>
         );
