c4095f2f341b1cbe9966fd34fd1310bfc62e0c76
AbdElKhalekA <AbdElKhalekA@E0410830>
2018-12-16 14:25:37 +0200

    modify header component to be stateless fuction

--- src/modules/shared/header.js
+++ src/modules/shared/header.js
@@ -2,33 +2,31 @@ import React from 'react';
 
 import { Link } from 'react-router-dom';
 
-export class Header extends React.Component {
-    render() {
-        return (
-            <div>
-               
-                <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="#">Wuilt Task</a>
+function Header() {
+    return (
+        <div>
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                <Link to="/home" style={{ color: 'White' }}>Wuilt Task</Link>
+                <div class="collapse navbar-collapse" id="navbarText">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item active">
+                            <Link class="nav-link" to="/home">Home</Link>
+                        </li>
+                        <li class="nav-item">
+                            <Link class="nav-link" to="/about">About</Link>
+                        </li>
+                        <li class="nav-item">
+                            <Link class="nav-link" to="/index">Users</Link>
+                        </li>
+                    </ul>
 
-  <div class="collapse navbar-collapse" id="navbarText">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-       <Link class="nav-link" to="/home">Home</Link>
-      </li>
-      <li class="nav-item">
-      <Link class="nav-link"  to="/about">About</Link>
-      </li>
-      <li class="nav-item">
-      <Link class="nav-link"  to="/index">Users</Link>
-      </li>
-    </ul>
-  
-  </div>
-</nav>
-            </div>
+                </div>
+            </nav>
+        </div>
 
 
-        )
-    }
+    )
 }
+
+export default Header;
 
